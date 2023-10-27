4f12e702feb01897504ad9c41f9a718790b37474
Amael <amael.muzeau@gmail.com>
2023-10-27 22:26:48 +0200

    Que les fichiers de base

--- README.md
+++ README.md
@@ -1,27 +1,14 @@
-# Data FullStack Application
+# Tp API
 
-[![Join the chat at https://gitter.im/fullstack-data-application/DSIA-5102A-2021](https://badges.gitter.im/fullstack-data-application/DSIA-5102A-2021.svg)](https://gitter.im/fullstack-data-application/DSIA-5102A-2021?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)
+Le but de ce TP est d'utiliser tout ce que vous avez vu en cours pour créer votre propre API.
+## Exercices
 
-![FullStack](docs/modern_web_app_with_auth.png)
-
-Pour commencer :
-
-## [Docker](docker/DOCKER.md)
-
-Pour l'ensemble des parties du cours
-
-## API 
-### [Cours](api/README.md)  
-### [TP](api/tp/README.md)
-
-## Front 
-### [Cours](front/README.md)  
-### [TP](front/tp/README.md)
-
-## Authentification : Keycloak
-### [Cours](authentication/README.md)  
-### [TP](authentication/tp/README.md)
-
-## Api Gateway : Kong
-### [Cours](kong/README.md)
-### [TP](kong/tp/README.md)
+1. Ajouter une route permettant de renvoyer la date actuelle
+2. Créer un schéma d'un objet de votre choix
+3. Ajouter le branchement avec la base de données.
+4. Ajouter le modèle SQLAlchemy
+5. Ajouter les routes CRUDs permettant de créer, modifier, supprimer un objet de votre choix
+6. Créer des services permettant de réaliser toutes ces opérations
+7. Remplacer le code dans les routes pour utiliser directement les services
+8. Ajouter une route permettant de lire un header, realiser une requête sur votre API via votre navigateur préféré et renvoyer le header correspondant
+ 
---
+++ app/main.py
@@ -0,0 +1,123 @@
+from pydantic import BaseModel
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+##################
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, Session
+import os
+from sqlalchemy import Column, String, Integer
+from fastapi import HTTPException, Depends
+
+
+class Book(BaseModel):
+    name: str
+    price: float
+    class Config:
+        orm_mode = True
+
+POSTGRES_USER = os.environ.get("POSTGRES_USER")
+POSTGRES_USER = os.environ.get("POSTGRES_USER")
+POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
+POSTGRES_DB = os.environ.get("POSTGRES_DB")
+
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@db/{POSTGRES_DB}"
+print(SQLALCHEMY_DATABASE_URL)
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=True, bind=engine)
+
+BaseSQL = declarative_base()
+
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
+
+class BooksDB(BaseSQL):
+    __tablename__ = "books"
+    name = Column(String, primary_key=True)
+    price = Column(Integer)
+    
+    class Config:
+        orm_mode = True
+
+##########################
+
+
+
+
+app = FastAPI(
+    title="My title",
+    description="My description",
+    version="0.0.1",
+)
+
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/TEST")
+def read_test():
+    return{"test":POSTGRES_USER}
+
+@app.on_event("startup")
+async def startup_event():
+    BaseSQL.metadata.create_all(bind=engine)
+
+@app.post("/books/")
+async def create_book(book: Book, db: Session = Depends(get_db)):
+    record = db.query(BooksDB).filter(BooksDB.name == book.name).first()
+    if record:
+        raise HTTPException(status_code=409, detail="Already exists")
+    db_book = BooksDB(**book.dict())
+    db.add(db_book)
+    db.commit()
+    db.refresh(db_book)
+    return db_book
+
+@app.get("/books/{name}")
+async def get_book_by_name(name: str, db: Session = Depends(get_db)):
+    record = db.query(BooksDB).filter(BooksDB.name == name).first()
+    print(name)
+    if not record:
+        raise HTTPException(status_code=404, detail="Not Found") 
+    return record
+
+@app.get("/all_books")
+async def get_all_books(db: Session = Depends(get_db)):
+    return db.query(BooksDB).all()
+
+@app.delete("/delete/{name}", tags=["posts"])
+async def delete_by_name(name: str, db: Session = Depends(get_db)):
+    try:
+        num_rows = db.query(BooksDB).filter_by(name=name).delete()
+        if num_rows==0:
+            raise HTTPException(status_code=404, detail="Record not found")
+        db.commit()
+    except HTTPException as e:
+        raise
+    except Exception as e:
+        return{"error":e}
+    return{"book": f"delete book {name}"}
+
+@app.delete("/delete_all")
+async def delete_all_books(db: Session = Depends(get_db)):
+    try:
+        num_rows = db.query(BooksDB).filter().delete()
+        if num_rows==0:
+            raise HTTPException(status_code=404, detail="Record not found")
+        db.commit()
+    except HTTPException as e:
+        raise
+    except Exception as e:
+        return{"error":e}
+    return{"book": f"delete all books"}
