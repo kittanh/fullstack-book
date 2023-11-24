b42eff7340871fcee53ba5a542addf2abd97b345
Amael <amael.muzeau@gmail.com>
2023-11-24 09:29:25 +0100

    test csv base de données

--- app/main.py
+++ app/main.py
@@ -7,13 +7,20 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 import os
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, Integer, Float
 from fastapi import HTTPException, Depends
 
 
 class Book(BaseModel):
-    name: str
-    price: float
+    id: int
+    title: str
+    authors: str
+    average_rating: float
+    language_code: str
+    num_pages: int
+    rating_count: int
+    text_review_count: int
+    publication_date: str
     class Config:
         orm_mode = True
 
@@ -40,10 +47,46 @@ def get_db():
     finally:
         db.close()
 
+def init_db():
+    db = SessionLocal()
+    new_book = BooksDB(id=0, title="TEST1", authors="M TEST",
+                       average_rating=4.5, language_code="FR",
+                       num_pages=100, rating_count=2,
+                       text_review_count=0, publication_date="12/01/2000")
+    db.add(new_book)
+    db.commit()
+    new_book = BooksDB(id=1, title="TEST2", authors="M TEST",
+                       average_rating=4.5, language_code="FR",
+                       num_pages=100, rating_count=2,
+                       text_review_count=0, publication_date="12/01/2000")
+    db.add(new_book)
+    db.commit()
+
+    # # Fonctionne mais problèmes plusieurs fois le même titre
+    # db = SessionLocal()
+    # with open("books.csv") as file:
+    #     next(file)  # On saute la première ligne header
+    #     for line in file:
+    #         line_info = line.split(",")                
+    #         if len(line_info)==12:            
+    #             new_book = BooksDB(id=int(line_info[0]), title=line_info[1], authors=line_info[2],
+    #                             average_rating=float(line_info[3]), language_code=line_info[6],
+    #                             num_pages=int(line_info[7]), rating_count=int(line_info[8]),
+    #                             text_review_count=int(line_info[9]), publication_date=line_info[10])
+    #             db.add(new_book)
+    #             db.commit()
+
 class BooksDB(BaseSQL):
     __tablename__ = "books"
-    name = Column(String, primary_key=True)
-    price = Column(Integer)
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    authors = Column(String)
+    average_rating = Column(Float)
+    language_code = Column(String)
+    num_pages = Column(Integer)
+    rating_count = Column(Integer)
+    text_review_count = Column(Integer)
+    publication_date = Column(String)
     
     class Config:
         orm_mode = True
@@ -59,23 +102,19 @@ app = FastAPI(
     version="0.0.1",
 )
 
-
-
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/TEST")
-def read_test():
-    return{"test":POSTGRES_USER}
-
 @app.on_event("startup")
 async def startup_event():
     BaseSQL.metadata.create_all(bind=engine)
+    if not SessionLocal().query(BooksDB).first():
+        init_db()
 
 @app.post("/books/")
 async def create_book(book: Book, db: Session = Depends(get_db)):
-    record = db.query(BooksDB).filter(BooksDB.name == book.name).first()
+    record = db.query(BooksDB).filter(BooksDB.id == book.id).first()
     if record:
         raise HTTPException(status_code=409, detail="Already exists")
     db_book = BooksDB(**book.dict())
@@ -84,10 +123,9 @@ async def create_book(book: Book, db: Session = Depends(get_db)):
     db.refresh(db_book)
     return db_book
 
-@app.get("/books/{name}")
-async def get_book_by_name(name: str, db: Session = Depends(get_db)):
-    record = db.query(BooksDB).filter(BooksDB.name == name).first()
-    print(name)
+@app.get("/books/{id}")
+async def get_book_by_id(id: int, db: Session = Depends(get_db)):
+    record = db.query(BooksDB).filter(BooksDB.id == id).first()
     if not record:
         raise HTTPException(status_code=404, detail="Not Found") 
     return record
@@ -96,10 +134,10 @@ async def get_book_by_name(name: str, db: Session = Depends(get_db)):
 async def get_all_books(db: Session = Depends(get_db)):
     return db.query(BooksDB).all()
 
-@app.delete("/delete/{name}", tags=["posts"])
-async def delete_by_name(name: str, db: Session = Depends(get_db)):
+@app.delete("/delete/{id}", tags=["posts"])
+async def delete_by_id(id: int, db: Session = Depends(get_db)):
     try:
-        num_rows = db.query(BooksDB).filter_by(name=name).delete()
+        num_rows = db.query(BooksDB).filter_by(id=id).delete()
         if num_rows==0:
             raise HTTPException(status_code=404, detail="Record not found")
         db.commit()
@@ -107,7 +145,7 @@ async def delete_by_name(name: str, db: Session = Depends(get_db)):
         raise
     except Exception as e:
         return{"error":e}
-    return{"book": f"delete book {name}"}
+    return{"book": f"delete book {id}"}
 
 @app.delete("/delete_all")
 async def delete_all_books(db: Session = Depends(get_db)):
--- front/front_api.py
+++ front/front_api.py
@@ -10,8 +10,15 @@ app = dash.Dash(__name__)
 
 app.layout = html.Div(children=[
     html.H1(children="Test API book"),
-    dbc.Row([dbc.Input(id="book_name", placeholder="Nom du livre"),
-             dbc.Input(id='book_price', placeholder="Prix du livre (entier)")]),
+    dbc.Row([dbc.Input(id="book_id", placeholder="ID du livre (int)"),
+             dbc.Input(id='book_title', placeholder="Titre du livre (str)"),
+             dbc.Input(id='book_authors', placeholder="Nom de l'autheur"),
+             dbc.Input(id='book_average_rating', placeholder="Note moyenne du livre (float)"),
+             dbc.Input(id='book_language_code', placeholder="Code de la langue du livre"),
+             dbc.Input(id='book_num_pages', placeholder="Nombre de pages (int)"),
+             dbc.Input(id='book_rating_count', placeholder="Nombre de notes (int)"),
+             dbc.Input(id='book_review_count', placeholder="Nombre de revus du livre (int)"),
+             dbc.Input(id='book_publication_date', placeholder="Date de publication du livre (int)"),]),
     dbc.Button("POST", id="post_button", n_clicks=0),
     html.H2(id="output_post"),
     dbc.Button("GET_ALL_BOOKS", id="all_books_button", n_clicks=0),
@@ -23,13 +30,26 @@ app.layout = html.Div(children=[
 @app.callback(
     Output("output_post", "children"),
     Input("post_button", "n_clicks"),
-    State("book_price", "value"),
-    State("book_name", "value"),
-    prevent_initial_call=True
+    State("book_id", "value"),
+    State("book_title", "value"),
+    State("book_authors", "value"),
+    State("book_average_rating", "value"),
+    State("book_language_code", "value"),
+    State("book_num_pages", "value"),
+    State("book_rating_count", "value"),
+    State("book_review_count", "value"),
+    State("book_publication_date", "value"),
 
+    prevent_initial_call=True
 )
-def post_book(n, price, name):
-    r = requests.post("http://api:5000/books/", json={"name": name, "price": price})
+def post_book(n, id, title, authors, average_rating, language_code, num_pages, rating_count,
+               review_count, publication_date):
+    r = requests.post("http://api:5000/books/",
+                      json={"id": id, "title": title, "authors": authors,"average_rating": average_rating,
+                            "language_code": language_code,"num_pages": num_pages,
+                            "rating_count": rating_count,"text_review_count": review_count,
+                            "publication_date": publication_date
+                            })
     return str(r.json())
 
 @app.callback(
